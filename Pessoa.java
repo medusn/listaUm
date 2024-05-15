72ea18ba78814e52095499e694c036b7f960560a
medusn <168425368+medusn@users.noreply.github.com>
2024-05-15 10:42:36 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,33 @@
+public class Pessoa {
+	private String  nome;
+	private int idade;
+	private double altura;
+	
+	public Pessoa(String novoNome, int novaIdade, double novaAltura) {
+		this.nome = novoNome;
+		this.idade = novaIdade;
+		this.altura = novaAltura;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	public void setIdade(int novaIdade) {
+		this.idade = novaIdade;
+	}
+	public void setAltura(double novaAltura) {
+		this.altura = novaAltura;	
+	}
+	public String impimir() {
+		return "Nome: " + this.nome + "\nidade: " + this.idade + "anos\nAltura: " + this.altura + "m";
+	}
+}
