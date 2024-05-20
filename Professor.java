88bfbdccfd6767a3c8a78d3a4c8d8397282dfe52
medusn <168425368+medusn@users.noreply.github.com>
2024-05-20 09:30:18 -0300

    Create Professor.java

---
+++ Professor.java
@@ -0,0 +1,29 @@
+public class Professor {
+	private String nome;
+	private String departamento;
+	private String titulacao;
+
+	public Professor(String novoNome, String novoDepartamento, String novaTitulacao) {
+		this.nome = novoNome;
+		this.departamento = novoDepartamento;
+		this.titulacao = novaTitulacao;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getDepartamento() {
+		return departamento;
+	}
+	public String getTitulacao() {
+		return titulacao;
+	}
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	public void setDepartamento(String novoDepartamento) {
+		this.departamento = novoDepartamento;
+	}
+	public void setTitulacao(String novaTitulacao) {
+		this.titulacao = novaTitulacao;
+	}
+}
