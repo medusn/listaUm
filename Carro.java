6d7111cfb7db20a907aa20c566b39b2825e477c8
medusn <168425368+medusn@users.noreply.github.com>
2024-05-20 09:22:29 -0300

    Create Carro.java

---
+++ Carro.java
@@ -0,0 +1,37 @@
+public class Carro {
+	private String modelo;
+	private String marca;
+	private int ano;
+	private String placa;
+	
+	public Carro(String novoModelo, String novaMarca, int novoAno, String novaPlaca) {
+		this.modelo = novoModelo;
+		this.marca = novaMarca;
+		this.ano = novoAno;
+		this.placa = novaPlaca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public String getPlaca() {
+		return placa;
+	}
+	public void setModelo(String novoModelo) {
+		this.modelo = novoModelo;
+	}
+	public void setMarca(String novaMarca) {
+		this.marca = novaMarca;
+	}
+	public void setAno(int novoAno) {
+		this.ano = novoAno;
+	}
+	public void setPlaca(String novaPlaca) {
+		this.placa = novaPlaca;
+	}
+}
