52f29443c40699aec5590b65b6623223355ab8a1
medusn <168425368+medusn@users.noreply.github.com>
2024-05-20 09:28:36 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,29 @@
+public class Aluno {
+	private String nome;
+	private String matricula;
+	private String curso;
+	
+	public Aluno(String novoNome, String novaMatricula, String novoCurso) {
+		this.nome = novoNome;
+		this.matricula = novaMatricula;
+		this.curso = novoCurso;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	public void setMatricula(String novaMatricula) {
+		this.matricula = novaMatricula;
+	}
+	public void setCurso(String novoCurso) {
+		this.curso = novoCurso;
+	}
+}
